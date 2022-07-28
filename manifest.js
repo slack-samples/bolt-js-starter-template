aaacef0cc0ddc4b1630f965493230aa83bdf7f62
Sarah Jiang <sarahjiang@slack-corp.com>
2022-07-28 11:05:39 -0700

    Update manifest descript and app home setting

--- manifest.js
+++ manifest.js
@@ -1,4 +1,4 @@
-const { DefineFunction, Manifest, Schema } = require('@slack/bolt'); // will be node module import
+const { DefineFunction, Manifest, Schema } = require('@slack/bolt');
 
 const ReverseFunction = DefineFunction({
   callback_id: 'reverse',
@@ -29,8 +29,8 @@ module.exports = Manifest({
   runOnSlack: false,
   name: 'Bolt Template App',
   displayName: 'Bolt Template App',
-  description: 'Reverse a string',
-  longDescription: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget neque sed nibh efficitur fermentum et nec est. Pellentesque pulvinar leo purus, sit amet aliquam libero gravida vel. Vestibulum justo augue, elementum sit amet dignissim eget, porttitor id urna. Phasellus non nibh at tortor facilisis gravida et nec ex. Suspendisse potenti.',
+  description: 'A starter app template built with Bolt JS',
+  longDescription: 'Sometimes fate is like a small sandstorm that keeps changing directions. You change direction but the sandstorm chases you. You turn again, but the storm adjusts. Over and over you play this out, like some ominous dance with death just before dawn. Why? Because this storm isn’t something that blew in from far away, something that has nothing to do with you. This storm is you.',
   botScopes: ['channels:history', 'chat:write', 'commands'],
   tokenManagementEnabled: true,
   socketModeEnabled: true,
@@ -39,7 +39,7 @@ module.exports = Manifest({
     appHome: {
       homeTabEnabled: true,
       messagesTabEnabled: false,
-      messagesTabReadOnlyEnabled: false,
+      messagesTabReadOnlyEnabled: true,
     },
     botUser: {
       always_online: false,
