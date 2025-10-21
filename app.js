71978d5f491db6bdf2d88f115185d512b78b611c
Eden Zimbelman <eden.zimbelman@salesforce.com>
2025-10-20 20:18:25 -0700

    style: import the named app instead of default bolt package

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-import bolt, { LogLevel } from '@slack/bolt';
+import { App, LogLevel } from '@slack/bolt';
 import { config } from 'dotenv';
 import { registerListeners } from './listeners/index.js';
 
 config();
 
 /** Initialization */
-const app = new bolt.App({
+const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   socketMode: true,
   appToken: process.env.SLACK_APP_TOKEN,
