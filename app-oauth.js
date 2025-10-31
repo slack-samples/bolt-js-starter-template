67f571f75c59ee90149e14f4f7f907af65870a41
Alissa Renz <alissa.renz@gmail.com>
2025-10-30 18:20:39 -0700

    feat: update OAuth code sample to use FileInstallationStore (#126)

--- app-oauth.js
+++ app-oauth.js
@@ -1,11 +1,8 @@
-import { App, LogLevel } from '@slack/bolt';
+import { App, FileInstallationStore, LogLevel } from '@slack/bolt';
 import { config } from 'dotenv';
 import { registerListeners } from './listeners/index.js';
 
 config();
-
-// For development purposes only
-const tempDB = new Map();
 
 const app = new App({
   logLevel: LogLevel.DEBUG,
@@ -14,41 +11,9 @@ const app = new App({
   clientSecret: process.env.SLACK_CLIENT_SECRET,
   stateSecret: 'my-state-secret',
   scopes: ['channels:history', 'chat:write', 'commands'],
-  installationStore: {
-    storeInstallation: async (installation) => {
-      // Org-wide installation
-      if (installation.isEnterpriseInstall && installation.enterprise !== undefined) {
-        return tempDB.set(installation.enterprise.id, installation);
-      }
-      // Single team installation
-      if (installation.team !== undefined) {
-        return tempDB.set(installation.team.id, installation);
-      }
-      throw new Error('Failed saving installation data to installationStore');
-    },
-    fetchInstallation: async (installQuery) => {
-      // Org-wide installation lookup
-      if (installQuery.isEnterpriseInstall && installQuery.enterpriseId !== undefined) {
-        return tempDB.get(installQuery.enterpriseId);
-      }
-      // Single team installation lookup
-      if (installQuery.teamId !== undefined) {
-        return tempDB.get(installQuery.teamId);
-      }
-      throw new Error('Failed fetching installation');
-    },
-    deleteInstallation: async (installQuery) => {
-      // Org-wide installation deletion
-      if (installQuery.isEnterpriseInstall && installQuery.enterpriseId !== undefined) {
-        return tempDB.delete(installQuery.enterpriseId);
-      }
-      // Single team installation deletion
-      if (installQuery.teamId !== undefined) {
-        return tempDB.delete(installQuery.teamId);
-      }
-      throw new Error('Failed to delete installation');
-    },
-  },
+  // FileInstallationStore is intended for development purposes only
+  // Learn more: https://docs.slack.dev/tools/bolt-js/concepts/authenticating-oauth#installation-store
+  installationStore: new FileInstallationStore(),
   installerOptions: {
     // If true, /slack/install redirects installers to the Slack Authorize URL
     // without rendering the web page with "Add to Slack" button
